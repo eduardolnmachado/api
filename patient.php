0c50fb9f6aed7232643483de32305c4b96635b26
Eduardo Machado <eduardo.ln.machado@gmail.com>
2019-11-12 09:31:43 -0300

    Change parameters

--- patient.php
+++ patient.php
@@ -4,7 +4,7 @@ header('Content-Type: application/json; charset=utf-8');
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     
-    $patient = array(array('Nome' => 'Nome do Usuário', 'Idade' => '37', 'Cargo' => 'Nome do cargo', 'Area' => 'TI', 'CPF' => '123.456.789-11'));
+    $patient = array(array('Nome' => 'Nome do Usuário', 'Idade' => '40', 'Cargo' => 'Nome do cargo', 'Area' => 'TI', 'CPF' => '123.456.789-11'));
     
     echo json_encode($patient);
 } else {
